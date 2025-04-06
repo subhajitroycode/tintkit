0431756a4de210d7b73864d138e50fec253a38aa
Subhajit Roy <roysubhajit8697@gmail.com>
2025-04-06 19:56:52 +0530

    fixed border color

--- src/components/UISamples/Buttons/ButtonContainer.jsx
+++ src/components/UISamples/Buttons/ButtonContainer.jsx
@@ -5,7 +5,7 @@ const ButtonContainer = ({ variant, colorType }) => {
   const buttons = ["Default", "Hover", "Active", "Disabled"];
 
   return (
-    <div className="border rounded-lg p-4 shadow-sm">
+    <div className="border border-neutral-300 dark:border-neutral-600 rounded-lg p-4 shadow-sm">
       <h3 className="font-medium space-y-4 mb-4">Buttons · {variant}</h3>
       <div className="flex flex-col space-y-4">
         {buttons.map((state) => (
