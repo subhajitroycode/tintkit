0409c0280491523c191e2584cc8a0d3a5b3a2b87
Subhajit Roy <roysubhajit8697@gmail.com>
2025-04-30 21:16:51 +0530

    Refactor CardContainer to import Card components from a centralized index file

--- src/components/UISamples/CardComponent/CardContainer.jsx
+++ src/components/UISamples/CardComponent/CardContainer.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { nanoid } from "nanoid";
-import Card1 from "./Cards/Card1";
-import Card2 from "./Cards/Card2";
-import Card3 from "./Cards/Card3";
-import Card4 from "./Cards/Card4";
-import Card5 from "./Cards/Card5";
-import Card6 from "./Cards/Card6";
-import Card7 from "./Cards/Card7";
+import { Card1, Card2, Card3, Card4, Card5, Card6, Card7 } from "./Cards";
 
 const cards = [Card1, Card2, Card3, Card4, Card5, Card6, Card7];
 
---
+++ src/components/UISamples/CardComponent/Cards/index.js
@@ -0,0 +1,9 @@
+import Card1 from "./Card1";
+import Card2 from "./Card2";
+import Card3 from "./Card3";
+import Card4 from "./Card4";
+import Card5 from "./Card5";
+import Card6 from "./Card6";
+import Card7 from "./Card7";
+
+export { Card1, Card2, Card3, Card4, Card5, Card6, Card7 };
